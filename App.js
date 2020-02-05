ecff45b5f627073032a92334b3d82eafabeb6177
Hải Đăng Trần <dangth@dichung.vn>
2020-02-05 16:32:50 +0700

    add Push notification

--- App.js
+++ App.js
@@ -11,8 +11,28 @@ import React,{ Component } from 'react';
 import Router from './src/Router'
 import { Provider } from 'react-redux'
 import store from './src/core/Redux/store'
+import firebase from 'react-native-firebase'
 
 class App extends Component {
+  async componentDidMount() {
+    const enable = await firebase.messaging().hasPermission();
+    if (enable) {
+      const fmcToken = await firebase.messaging().getToken()
+      console.log('fmcToken', fmcToken);
+
+      firebase.notifications().onNotification(notification => {
+        alert(notification.body)
+        console.log(notification.body)
+      })
+    } else {
+      try {
+        firebase.messaging().requestPermission();
+      }
+      catch (e) {
+        alert('use rejected the permissions');
+      }
+    }
+  }
   render() {
     return (
       <Provider store={store}>
