b9f3b04a29514e2187041fc46c458609748b0707
dagcoi <Haidang261096@gmail.com>
2020-08-06 15:19:18 +0700

    Login Auth Phone

--- android/app/src/main/java/vn.dichung/hubapp/MainApplication.java
+++ android/app/src/main/java/vn.dichung/hubapp/MainApplication.java
@@ -13,7 +13,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-// import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 
@@ -32,7 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          // packages.add(new RNFirebaseAuthPackage());
+          packages.add(new RNFirebaseAuthPackage());
           packages.add(new RNFirebaseMessagingPackage());
           packages.add(new RNFirebaseNotificationsPackage());
           return packages;
